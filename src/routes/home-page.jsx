25064d7ed15debc8112a0c85d76da3792fa3d8fd
molor824 <molormargaderdene@gmail.com>
2024-11-10 22:08:14 +0800

    feat: create title for home page

--- src/routes/home-page.jsx
+++ src/routes/home-page.jsx
@@ -1,8 +1,10 @@
 export default function HomePage() {
   return (
-    <section className="section-container flex flex-col items-center">
-      <div className="font-bold text-4xl">Home</div>
-      <p className="text-2xl">Welcome to Hexashop</p>
-    </section>
+    <>
+      <div className="title-bg w-full h-[400px] flex flex-col items-center justify-center gap-4">
+        <h1 className="text-white font-bold text-6xl">Check Our Products</h1>
+      </div>
+      <section className="section-container flex flex-col items-center"></section>
+    </>
   );
 }
